7697fd12e9405693c73d654ec848afbf3cbca8a0
Daniel Lee <bearlee@alum.mit.edu>
2015-05-26 16:31:02 -0400

    fixing integrate_ode.hpp to pass tests

--- src/stan/math/prim/arr/functor/integrate_ode.hpp
+++ src/stan/math/prim/arr/functor/integrate_ode.hpp
@@ -122,7 +122,7 @@ namespace stan {
       stan::math::check_nonzero_size("integrate_ode", "times", ts);
       stan::math::check_nonzero_size("integrate_ode", "initial state", y0);
       stan::math::check_ordered("integrate_ode", "times", ts);
-      //stan::math::check_less("integrate_ode", "initial time", t0, ts[0]);
+      stan::math::check_less("integrate_ode", "initial time", t0, ts[0]);
 
       const double absolute_tolerance = 1e-12;
       const double relative_tolerance = 1e-9;
